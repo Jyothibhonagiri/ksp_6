d463128a7e88041c5094c588779dd17db0aaf354
Jyothibhonagiri <142475505+Jyothibhonagiri@users.noreply.github.com>
2023-09-17 10:31:20 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,44 @@
+print("hello world")
+#string methods
+name="jyothi"
+print(str(name))
+print(len(name))
+print(type(name))
+#index
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+for i in range(1,11):
+    print(i)
+#if condition
+a=12
+b=15
+if a>b:
+    print("a is small")
+#if else
+c=2
+d=4
+if c<d:
+    print("c is lesser number")
+else:
+    print("c is greater number")
+#elif
+d1=5
+d2=6
+if d1>d2:
+    print("hello")
+elif d1==d2:
+    print("welcome")
+else:
+    print("to the class")
+#functions
+def age():
+    print("hi welcome")
+    print("kick start program")
+age()
+    
+
+
